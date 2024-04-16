78639bd6281d9490f54f4c22354db9c2e6b533ed
SumiT0998 <sumit.bisht36@gmail.com>
2024-04-17 05:01:18 +0530

    make new change

--- week1/js_foundation/main.js
+++ week1/js_foundation/main.js
@@ -1,10 +1,23 @@
 // let ismarried=true;
 
+<<<<<<< HEAD
 // if(ismarried==true){console.log("sumit is married")}                     //if/else
 // else{console.log("not married")}
 
+=======
+
+// if(ismarried==true){console.log("sumit is married")}                     //if/else
+// else{console.log("not married")}
+
+
+>>>>>>> 848645a (v2.0)
 // let sum=0;
 // for(let i=0;i<=3;i++){
 //     sum+=i;                           ////////////////////////////////////  for loop
 // }
 // console.log(sum);
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 848645a (v2.0)
